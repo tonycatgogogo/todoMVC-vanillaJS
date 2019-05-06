99ee9baedad7640f962fab3b578df7d29b2439d5
tonycatgogogo <gonghaitao1@gmail.com>
2019-05-06 12:57:48 +0800

    after build

--- origin_code/webpack.config.js
+++ origin_code/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-
+const uglify = require('uglifyjs-webpack-plugin');
 module.exports = {
   mode: 'development',
   module: {
@@ -21,6 +21,7 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: path.join(__dirname, './src/index.html'),
       filename: "./index.html"
-    })
+    }),
+    new uglify(),
   ]
 };
